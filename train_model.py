6c0dce1c44e2d40e3c8bc87570fc8640f0abe36f
Ethan Coco <ethancoco1@outlook.com>
2023-04-27 21:21:49 -0400

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -4,16 +4,19 @@ import os
 import pandas as pd
 import random
 
-import tensorflow
 import tensorflow as tf
+from keras import regularizers
+from keras.layers import BatchNormalization
+from keras.utils import to_categorical
 from tensorflow import keras
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, LSTM
 
-dataset_dir = 'C:/Users/ethan/Desktop/ASLDataset/training_data/' #Dataset local directory
-testing_dataset_dir = 'C:/Users/ethan/Desktop/ASLDataset/testing_data/' #Testing Dataset local directory
-categories = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'] #List of data categories
+dataset_dir = 'C:/Users/ethan/Desktop/AVRDataset/asl/training_set_normalized/' #Dataset local directory
+testing_dataset_dir = 'C:/Users/ethan/Desktop/AVRDataset/asl/testing_set_normalized/' #Testing Dataset local directory
+#categories = ['a','b','c','d','e','f','g','h','i','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y'] #List of data categories #j and z out
 #categories = ['a','b','c']
+categories = ['a','b','c','d','e','f','g','h','i','k','l','o','s','w','x','y']
 
 training_data = []
 testing_data = []
@@ -31,7 +34,15 @@ for category in categories:
         file_path = os.path.join(path, file)
         data = pd.read_csv(file_path)
         data = data.values
-        training_data.append([data[:, 2:22], category_num])
+
+        #data[:, 2:7][data[:, 2:7] < 0] = 0
+        #data[:, 2:7][data[:, 2:7] > 100] = 100
+        #data[:, 2:7] = data[:, 2:7] / 100
+
+        #training_data.append([data[:, 2:7], category_num])
+        training_data.append([data[:, 1:6], category_num])
+
+#print(training_data)
 
 for category in categories:
     path = os.path.join(testing_dataset_dir, category)  # Path to categories
@@ -40,7 +51,12 @@ for category in categories:
         file_path = os.path.join(path, file)
         data = pd.read_csv(file_path)
         data = data.values
-        testing_data.append([data[:, 2:22], category_num])
+
+        #data[:, 2:7][data[:, 2:7] < 0] = 0
+        #data[:, 2:7][data[:, 2:7] > 100] = 100
+        #data[:, 2:7] = data[:, 2:7] / 100
+
+        testing_data.append([data[:, 1:6], category_num]) #2:22
 
 random.shuffle(training_data)
 random.shuffle(testing_data)
@@ -59,29 +75,61 @@ label_set = np.array(label_set)
 feature_test_set = np.array(feature_test_set)
 label_test_set = np.array(label_test_set)
 
-feature_set = np.array(feature_set).reshape(-1, 1500, 20)
-#label_set = np.array(label_set)
-#print(label_set.shape)
+print(feature_set.shape)
+print(feature_test_set.shape)
+
+feature_set = np.array(feature_set).reshape(-1, 1500, 5) #(-1, 1500, 20)
 label_set = np.array(label_set).reshape(-1,)
+
+feature_test_set = np.array(feature_test_set).reshape(-1, 450, 5)
+label_test_set = np.array(label_test_set).reshape(-1,)
+
+#feature_set = to_categorical(feature_set, 24)
+label_set = to_categorical(label_set, 16)
+#label_set = to_categorical(label_set, 26)
+#feature_test_set = to_categorical(feature_set, 24)
+label_test_set = to_categorical(label_test_set, 16)
+#label_test_set = to_categorical(label_test_set, 26)
 
 #use pickle here
 
 model = Sequential()
-model.add(keras.Input(shape=(1500, 20)))
-model.add(keras.layers.SimpleRNN(128, return_sequences=True, activation='relu'))
-model.add(keras.layers.SimpleRNN(128, return_sequences=False, activation='relu'))
-model.add(keras.layers.Dense(26))
+model.add(keras.layers.SimpleRNN(units=64, return_sequences=True, input_shape=(1500, 5), kernel_regularizer=regularizers.l2(0.01))) #activation='tanh'
+model.add(keras.layers.SimpleRNN(units=64, return_sequences=False, input_shape=(1500, 5), kernel_regularizer=regularizers.l2(0.01)))
+model.add(keras.layers.Dense(units=36))
+##model.add(keras.layers.Dense(units=16, activation='softmax'))
+model.add(keras.layers.Dense(units=16, activation='softmax'))
+#model.add(BatchNormalization())
+##model.add(keras.Input(shape=(1500, 5))) #(-1, 1500, 20)
+#model.add(BatchNormalization()) #
+##model.add(keras.layers.SimpleRNN(64, return_sequences=True, activation='relu', kernel_regularizer=regularizers.l2(0.01))) #128#relu #kernel_regularizer=regularizers.l2(0.01) #SimpleRNN
+#model.add(keras.layers.Dropout(0.1))
+##model.add(keras.layers.SimpleRNN(64, return_sequences=False, activation='relu', kernel_regularizer=regularizers.l2(0.01)))
+#model.add(keras.layers.Dropout(0.1))
+#model.add(keras.layers.Dense(36))
+#model.add(keras.layers.Dense(24))
+##model.add(keras.layers.Dense(16))
+#model.add(keras.layers.Dense(26, activation='softmax'))
 
-loss = keras.losses.SparseCategoricalCrossentropy(from_logits=True)
-optim = keras.optimizers.Adam(learning_rate=0.001)
-metrics = ["accuracy"]
+#loss = keras.losses.SparseCategoricalCrossentropy(from_logits=True) #from_logits=True
+#optim = keras.optimizers.Adam(learning_rate=0.001) #learning_rate=0.001
+#metrics = ["accuracy"]
+model.summary()
 
-model.compile(loss=loss, optimizer=optim, metrics=metrics)
+#model.compile(loss=loss, optimizer=optim, metrics=metrics)
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-batch_size = 64
-epochs = 20
+#batch_size = 25 #64
+epochs = 400
 
-model.fit(feature_set, label_set, batch_size = batch_size, epochs = epochs)
+#history = model.fit(feature_set, label_set, batch_size = batch_size, epochs = epochs)
+history = model.fit(feature_set, label_set, epochs = epochs)
+pd.DataFrame(history.history).plot(figsize=(8, 5))
+plt.grid(True)
+plt.gca().set_ylim(0, 1)
+#save_fig("keras_learning_curves_plot")
+plt.show()
+
 
 #model.evaluate()
 
